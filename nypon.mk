054f768731ebaba9f42a9005c36124bb6c2e1bdd
Arnav Gupta <championswimmer@gmail.com>
2012-10-30 14:33:18 -0700

    nypon:set  product lcd density
    Change-Id: If32390b25e50849328c1f5946c28b91b2906f34a
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- nypon.mk
+++ nypon.mk
@@ -88,3 +88,5 @@ $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/sony/nypon/nypon-vendor.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240
