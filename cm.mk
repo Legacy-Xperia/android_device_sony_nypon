06a42a43a7a62f315332cc826a67c9a5ac27e97e
Alin Jerpelea <jerpelea@gmail.com>
2013-03-02 21:26:24 +0200

    nypon: NFC fix
    Change-Id: I4b3ae4a589c639efda96aeb061b6b09ff29411e3

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST22i_1257-4009 BUILD_FINGERPRINT=SEMC/ST22i_1257-4009/ST22i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST22i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-540x960
