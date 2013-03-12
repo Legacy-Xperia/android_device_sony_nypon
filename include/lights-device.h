37c1a7826f88f2286d1092b632356c07403bf8f0
Alin Jerpelea <jerpelea@gmail.com>
2013-03-12 20:05:52 +0200

    nypon: liblights: tipo fix
    Change-Id: I85e83e5ecba74799fa6ecda77c759db02c7a4e0e

--- include/lights-device.h
+++ include/lights-device.h
@@ -1,12 +1,6 @@
-char const*const RED_LED_FILE[] = {
- "/sys/class/leds/red/brightness",
-};
-char const*const GREEN_LED_FILE[] = {
- "/sys/class/leds/green/brightness",
-};
-char const*const BLUE_LED_FILE[] = {
- "/sys/class/leds/blue/brightness",
-};
+char const*const RED_LED_FILE = "/sys/class/leds/red/brightness";
+char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
+char const*const BLUE_LED_FILE = "/sys/class/leds/blue/brightness";
 
 char const*const LED_FILE_TRIGGER[] = {
  "/sys/class/leds/red/trigger",
