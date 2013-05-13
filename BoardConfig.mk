db3283439518cc2e3fd1c48d6c27c8618437a810
Alin Jerpelea <jerpelea@gmail.com>
2013-05-13 18:48:41 +0300

    nypon: rename cm_defconfig
    Change-Id: I65135375bdc7e9ca39d19a48d2b6b23feb1c0313

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ USE_CAMERA_STUB := true
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/nypon/include
 
-TARGET_KERNEL_CONFIG := cyanogen_nypon_defconfig
+TARGET_KERNEL_CONFIG := cm_montblanc_nypon_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
