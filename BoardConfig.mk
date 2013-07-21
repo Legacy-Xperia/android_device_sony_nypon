712690bf925c8d7daf1d74394c74dc64032e78db
percy_g2 <gahlotpercy@gmail.com>
2013-07-22 01:20:07 +0530

    bulk update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,8 @@
 USE_CAMERA_STUB := true
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/nypon/bluetooth
+
 # inherit from the proprietary version
 -include vendor/sony/nypon/BoardConfigVendor.mk
 
