5752d3f1cbcb8a61fd7c4a901ad035926a609a81
Rebel <gahlotpercy@gmail.com>
2013-11-25 14:34:54 +0530

    Update nypon.mk

--- nypon.mk
+++ nypon.mk
@@ -41,7 +41,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.google.android.nfc_extras.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # Configs
 PRODUCT_COPY_FILES += \
@@ -254,8 +255,6 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 
 # Device specific sysmon_monitor conf
