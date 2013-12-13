4e7a8a35b51ee0c6413a663f73b7385663da290d
Percy Gahlot <gahlotpercy@gmail.com>
2013-12-13 14:19:47 +0530

    Update aosp_nypon.mk

--- aosp_nypon.mk
+++ aosp_nypon.mk
@@ -1,13 +1,4 @@
-$(call inherit-product, device/sony/nypon/full_nypon.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/unkown/config/gsm.mk)
-
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/unkown/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/unkown/config/nfc_enhanced.mk)
 
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT22i_1257-4009 BUILD_FINGERPRINT=SEMC/LT22i_1257-4009/LT22i:4.4.2/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="LT22i-user 4.4.2 6.1.A.0.452 O_5_zw test-keys"
