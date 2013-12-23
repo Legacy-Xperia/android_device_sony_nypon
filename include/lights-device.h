b645db135c8853437215fd2b17c863d73d12806b
Percy Gahlot <gahlotpercy@gmail.com>
2013-12-24 01:11:48 +0530

    errr

--- include/lights-device.h
+++ include/lights-device.h
@@ -3,20 +3,28 @@ char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
 char const*const BLUE_LED_FILE = "/sys/class/leds/blue/brightness";
 
 char const*const LED_FILE_TRIGGER[] = {
- "/sys/class/leds/red/use_pattern",
- "/sys/class/leds/green/use_pattern",
- "/sys/class/leds/blue/use_pattern",
+ "/sys/class/leds/red/trigger",
+ "/sys/class/leds/green/trigger",
+ "/sys/class/leds/blue/trigger",
+};
+
+char const*const LED_FILE_DELAYON[] = {
+ "/sys/class/leds/red/delay_on",
+ "/sys/class/leds/green/delay_on",
+ "/sys/class/leds/blue/delay_on",
+};
+
+char const*const LED_FILE_DELAYOFF[] = {
+ "/sys/class/leds/red/delay_off",
+ "/sys/class/leds/green/delay_off",
+ "/sys/class/leds/blue/delay_off",
 };
 
 char const*const BUTTON_BACKLIGHT_FILE[] = {
- "/sys/class/leds/button-backlight/brightness",
 };
+// "/sys/class/leds/so34-led0/brightness",
+// "/sys/class/leds/so34-led1/brightness",
+// "/sys/class/leds/so34-led2/brightness",
 
-char const*const LED_FILE_PATTERN = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_data";
-char const*const LED_FILE_DELAYON = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_delay";
-char const*const LED_FILE_DELAYOFF = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs";
-char const*const LED_FILE_DIMTIME = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_use_softdim";
-char const*const LED_FILE_DIMONOFF = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/dim_time";
-
-char const*const ON = "1";
-char const*const OFF = "0";
+char const*const ON = "timer";
+char const*const OFF = "timer";
